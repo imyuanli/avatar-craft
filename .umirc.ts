01227266d67d063b01c2f807223403eb1a303eba
yuanli <2865437316@qq.com>
2023-08-11 16:14:04 +0800

    fix: 移除router

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
-  routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
-  ],
-
   npmClient: "yarn",
   tailwindcss: {},
   plugins: ["@umijs/plugins/dist/tailwindcss"],
